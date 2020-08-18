aa258e5d2b4e5e08afc14065b74ff86871c7ff07
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2020-08-18 16:03:10 +0500

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,24 @@
+# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
+import smtplib
+
+import config
+
+
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+
+subject = ""
+msg = ""
+
+send_email(subject, msg)
